3837d940431959573634da343d24706eedf22dbd
AVP <aakpat6@gmail.com>
2021-09-07 15:01:09 -0500

    Remove unnecessary var

--- app/hrstep.js
+++ app/hrstep.js
@@ -1,18 +1,14 @@
 import document from "document";
 import { today } from "user-activity";
 
-let stepsThisHour = 0;
 let stepsOffset = today.adjusted.steps;
 
 export function getStepsThisHour() {
-  return stepsThisHour;
+  return today.adjusted.steps - stepsOffset;
 }
 
 export function updateStepsThisHour(date) {
-  if (date.getMinutes() === 0 && date.getSeconds() < 10) {
-    stepsThisHour = 0;
+  if (date.getMinutes() === 0 && date.getSeconds() < 5) {
     stepsOffset = today.adjusted.steps;
-  } else {
-    stepsThisHour = today.adjusted.steps - stepsOffset;
   }
 }
